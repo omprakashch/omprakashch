2d0c3f8dae5dae87bd5f229e71cb804291841fda
divya <divya@LAPTOP-DIP3NAVD>
2021-12-06 11:18:26 +0530

    Updated EventFiring WebDriver Steps

--- Target/src/main/java/com/target/qa/base/TestBase.java
+++ Target/src/main/java/com/target/qa/base/TestBase.java
@@ -9,14 +9,18 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.target.qa.util.TestUtil;
+import com.target.qa.util.WebEventListener;
 
 public class TestBase {
 	public static WebDriver driver;
 	public static Properties prop;
+	public static EventFiringWebDriver e_driver;
+	public static WebEventListener eventListener;
 	
 	public TestBase (){
 		try{
@@ -42,6 +46,11 @@ public class TestBase {
 			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/Drivers/Firefox/geckodriver.exe");
 		driver = new FirefoxDriver();
 		}
+		
+		e_driver = new EventFiringWebDriver(driver);
+		eventListener = new WebEventListener();
+		e_driver.register(eventListener);
+		driver = e_driver;
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT,TimeUnit.SECONDS);
