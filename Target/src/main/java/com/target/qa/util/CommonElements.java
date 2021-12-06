7f1bcde03d9421edda4677ca618023ff3e165878
divya <divya@LAPTOP-DIP3NAVD>
2021-12-06 11:21:59 +0530

    Added Common Elements & WebEventListener and Updated TestUtil

---
+++ Target/src/main/java/com/target/qa/util/CommonElements.java
@@ -0,0 +1,103 @@
+package com.target.qa.util;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.target.qa.pages.ClearancePage;
+import com.target.qa.pages.RedCardExclusivesPage;
+import com.target.qa.pages.TargetCircleOffersPage;
+import com.target.qa.pages.TopDealsPage;
+import com.target.qa.pages.WeeklyAdPage;
+import com.target.qa.util.TestUtil;
+
+public class CommonElements extends TestUtil{
+	
+	@FindBy(xpath = "//a[@id='categories'][2]")
+	WebElement Categories;
+	
+	@FindBy(xpath = "//a[@aria-label='deals menu']")
+	WebElement Deals;
+	
+	@FindBy(xpath = "//a[@id='trending']")
+	WebElement WhatsNew;
+	
+	@FindBy(xpath = "//a[@id='pickupAndDelivery']")
+	WebElement PickupAndDelivery;
+	
+	@FindBy(xpath = "//input[@id='search']")
+	WebElement SearchInputField;
+	
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement SearchIcon;
+	
+	@FindBy(xpath = "//div[text()='Clearance']")
+	WebElement Clearance;
+	
+	@FindBy(xpath = "//div[text()='Weekly Ad']")
+	WebElement WeeklyAd;
+	
+	@FindBy(xpath = "//div[text()='Top Deals']")
+	WebElement TopDeals;
+	
+	@FindBy(xpath = "//div[text()='RedCard Exclusives']")
+	WebElement RedCardExclusives;
+	
+	@FindBy(xpath = "//div[text()='Target Circle Offers']")
+	WebElement TargetCircleOffers;
+	
+	@FindBy(xpath = "//div[@id='footer']")
+	WebElement FooterSection;
+	
+	@FindBy(xpath = "//span[contains(text(),'Trending items')]/../../../../../../..")
+	WebElement TrendingItemsSection;
+	
+	@FindBy(xpath = "//span[contains(text(),'Trending items')]/../../../../../../../li")
+	List<WebElement> TrendingItemsList;
+	
+	
+	public CommonElements(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	public ClearancePage clickClearnce(){
+		click(Deals);
+		jseClick(Clearance);
+		pause(3);
+		return new ClearancePage();
+	}
+	
+	public WeeklyAdPage clickWeeklyAd(){
+		click(Deals);
+		jseClick(WeeklyAd);
+		pause(3);
+		return new WeeklyAdPage();
+	}
+	
+	public TopDealsPage clickTopDeals(){
+		click(Deals);
+		jseClick(TopDeals);
+		pause(3);
+		return new TopDealsPage();
+	}
+
+	public RedCardExclusivesPage clickRedCardExclusives(){
+		click(Deals);
+		jseClick(RedCardExclusives);
+		pause(3);
+		return new RedCardExclusivesPage();
+	}
+	
+	public TargetCircleOffersPage clickTargetCircleOffers(){
+		click(Deals);
+		jseClick(TargetCircleOffers);
+		pause(3);
+		return new TargetCircleOffersPage();
+	}
+	
+	public List<WebElement> getTrendingItemsList(){
+		scroll(10);
+		return TrendingItemsList;
+	}
+}
--- Target/src/main/java/com/target/qa/util/TestUtil.java
+++ Target/src/main/java/com/target/qa/util/TestUtil.java
@@ -1,7 +1,19 @@
 package com.target.qa.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 
 import com.target.qa.base.TestBase;
@@ -10,6 +22,17 @@ public class TestUtil extends TestBase {
 
 	public static long PAGE_LOAD_TIMEOUT = 60;
 	public static long IMPLICIT_WAIT = 25;
+	
+	static String currectDir = System.getProperty("user.dir");
+	public static String TESTDATA_SHEET_PATH = currectDir+"/src/main/java/com/target/qa/testdata/TargetTestData.xlsx";
+
+	static Workbook book;
+	static Sheet sheet;
+	static JavascriptExecutor js;
+	
+	public void switchToFrame() {
+		driver.switchTo().frame("mainpanel");
+	}
 	
 	public WebElement getElement(String objPath){
 		WebElement element = null;
@@ -26,25 +49,124 @@ public class TestUtil extends TestBase {
 		getElement(elementPath).click();
 	}
 	
+	public void click(WebElement element){
+		element.click();
+	}
+	
 	public String getText(String elementPath){
 		return getElement(elementPath).getText();
+	}
+	
+	public String getText(WebElement element){
+		return element.getText();
 	}
 	
 	public String getAttribute(String elementPath){
 		return getElement(elementPath).getAttribute("value");
 	}
 	
+	public String getAttribute(WebElement element){
+		return element.getAttribute("value");
+	}
+	
 	public void enterText(String elementPath, String inputText){
 		getElement(elementPath).sendKeys(inputText);
 	}
 	
-	public void scrollToAndClick(String elementPath){
+	public void enterText(WebElement element, String inputText){
+		element.sendKeys(inputText);
+	}
+	
+	public String getPageTitle(){
+		return driver.getTitle();
+	}
+	
+	public void pause(long sec){
+		try {
+			Thread.sleep(sec*1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void scrollToAndView(String elementPath){
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].scrollIntoView();", getElement(elementPath));
+	}
+	
+	public void scrollToAndView(WebElement element){
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true);",element);
+	}
+	
+	public void scroll(int iteration){
+		for(int i=1;i<=iteration;i++){
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,250)", "");
+		}
 	}
 	
 	public void jseClick(String elementPath){
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click();", getElement(elementPath));
+	}
+	
+	public void jseClick(WebElement element){
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", element);
+	}
+	
+	public boolean isElementPresent(String elementPath){
+		boolean flag = false;
+		try{
+			flag = getElement(elementPath).isDisplayed();
+		}
+		catch(Exception e){
+			flag = false;
+		}
+		return flag;
+	}
+	
+	public boolean isElementPresent(WebElement element){
+		boolean flag = false;
+		try{
+			flag = element.isDisplayed();
+		}
+		catch(Exception e){
+			flag = false;
+		}
+		return flag;
+	}
+	
+	public static Object[][] getTestData(String sheetName) {
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(TESTDATA_SHEET_PATH);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(file);
+		}  catch (IOException e) {
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		// System.out.println(sheet.getLastRowNum() + "--------" +
+		// sheet.getRow(0).getLastCellNum());
+		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+				// System.out.println(data[i][k]);
+			}
+		}
+		return data;
+	}
+
+	public static void takeScreenshotAtEndOfTest() throws IOException {
+	File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+	String currentDir = System.getProperty("user.dir");
+	FileUtils.copyFile(srcFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+		
 	}
 }
