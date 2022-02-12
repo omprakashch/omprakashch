2a00e19ab3ed632c8c600ccd1e86a474f0df409d
omprakashch <52399674+omprakashch@users.noreply.github.com>
2022-02-13 01:15:19 +0530

    Delete FreeCRMTest directory

--- FreeCRMTest/src/main/java/com/crm/qa/pages/ContactsPage.java
+++
@@ -1,6 +0,0 @@
-package com.crm.qa.pages;
-import com.crm.qa.util.TestUtil;
-
-public class ContactsPage extends TestUtil{
-
-}
--- FreeCRMTest/src/main/java/com/crm/qa/pages/SignUpPage.java
+++
@@ -1,6 +0,0 @@
-package com.crm.qa.pages;
-import com.crm.qa.util.TestUtil;
-
-public class SignUpPage extends TestUtil{
-
-}
